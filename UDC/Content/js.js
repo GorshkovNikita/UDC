bb51f91c272d9aa2dab6d1955fe145ccf6969761
Nikita Gorshkov <gorshkov.nikita.alexandrovich@gmail.com>
2014-12-11 23:43:30 +0400

    AddIndex

--- UDC/Content/js.js
+++ UDC/Content/js.js
@@ -5,3 +5,9 @@
     else
         node.style.display = "none";
 }
+
+function addMainIndex(elem) {
+    node1 = elem.nextSibling.nextSibling.children[0];
+    node2 = document.getElementsByClassName("main-table")[0];
+    node2.innerHTML = node1.innerHTML;
+}
